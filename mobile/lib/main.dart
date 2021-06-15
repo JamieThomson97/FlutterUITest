e5a49ffad001864f1caf46fe7d1d8f007a968e72
Jamie Thomson <jaytadmn@gmail.com>
2021-06-15 21:16:00 +0100

    login-y stuff
    still mostly bastardised from BloC docs

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:vibration/presentation/pages/artist_page.dart';
-import 'package:vibration/presentation/pages/home_page.dart';
-import 'package:vibration/presentation/pages/now_playing_page.dart';
-import 'package:vibration/presentation/pages/search_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vibration/bloc/app/app_bloc.dart';
 import 'package:vibration/presentation/pages/sign_in_page.dart';
+import 'package:vibration/repository/authentication_repository.dart';
 import 'package:vibration/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  final authenticationRepository = AuthenticationRepository();
+  await authenticationRepository.user.first;
+  runApp(MyApp(authenticationRepository: authenticationRepository));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+    required AuthenticationRepository authenticationRepository,
+  })   : _authenticationRepository = authenticationRepository,
+        super(key: key);
+
+  final AuthenticationRepository _authenticationRepository;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SignInPage(),
-      theme: theme,
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+          create: (_) => AppBloc(
+                authenticationRepository: _authenticationRepository,
+              ),
+          child: MaterialApp(
+            home: SignInPage(),
+            theme: theme,
+          )),
     );
   }
 }
