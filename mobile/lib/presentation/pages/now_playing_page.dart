1e827d9f2e62dc6287c946f256f7f81939312ee6
Jamie Thomson <jaytadmn@gmail.com>
2021-10-13 23:12:49 +0100

    idk still doesn't work

--- mobile/lib/presentation/pages/now_playing_page.dart
+++ mobile/lib/presentation/pages/now_playing_page.dart
@@ -17,152 +17,154 @@ class NowPlayingPage extends StatelessWidget {
     return SafeArea(
       child: Container(
         padding: EdgeInsets.all(6),
-        child: Stack(children: [
-          BlocBuilder<AudioControllerBloc, AudioControllerState>(
-            buildWhen: (prev, current) {
-              return current.secondsIn == 0;
-            },
-            builder: (context, state) {
-              return BlocProvider(
-                create: (context) => NowPlayingScrollCubit(
-                  _scrollController,
-                  state.mix!,
-                ),
-                child: BlocBuilder<NowPlayingScrollCubit, NowPlayingScrollState>(
-                  builder: (context, state) {
-                    return Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("resources/Now_Playing_Screen/KaytranadaLive.jpeg"),
-                          alignment: Alignment(state.songPercentage.abs() * 0.4, 0),
+        child: Stack(
+          children: [
+            BlocBuilder<AudioControllerBloc, AudioControllerState>(
+              buildWhen: (prev, current) {
+                return current.secondsIn == 0;
+              },
+              builder: (context, state) {
+                return BlocProvider(
+                  create: (context) => NowPlayingScrollCubit(
+                    _scrollController,
+                    state.mix!,
+                  ),
+                  child: BlocBuilder<NowPlayingScrollCubit, NowPlayingScrollState>(
+                    builder: (context, state) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("resources/Now_Playing_Screen/KaytranadaLive.jpeg"),
+                            alignment: Alignment(state.songPercentage.abs() * 0.4, 0),
+                          ),
                         ),
+                        padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BlocBuilder<AudioControllerBloc, AudioControllerState>(
+                          builder: (context, state) {
+                            return Container(
+                              color: Colors.white,
+                              child: MarqueeWidget(
+                                direction: Axis.horizontal,
+                                text: Text(
+                                  state.mix!.producer,
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        BlocBuilder<AudioControllerBloc, AudioControllerState>(
+                          // todo: buildWhen
+                          builder: (context, state) {
+                            return Container(
+                              color: Colors.white,
+                              child: MarqueeWidget(
+                                text: Text(
+                                  state.mix!.event,
+                                  style: Theme.of(context).textTheme.headline6,
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        BlocBuilder<AudioControllerBloc, AudioControllerState>(
+                          // todo: buildWhen
+                          builder: (context, state) {
+                            return Container(
+                              color: Colors.white,
+                              child: MarqueeWidget(
+                                text: Text(
+                                  DateFormat('yyyy-MM-dd').format(state.mix!.dateUploaded),
+                                  style: Theme.of(context).textTheme.headline5,
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  height: 300,
+                ),
+                SizedBox(
+                  height: 200,
+                ),
+                BlocBuilder<AudioControllerBloc, AudioControllerState>(
+                  buildWhen: (prev, current) {
+                    return current.secondsIn == 0;
+                  },
+                  builder: (context, state) {
+                    return BlocProvider(
+                      create: (context) => NowPlayingScrollCubit(
+                        _scrollController,
+                        state.mix!,
                       ),
-                      padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                      child: BlocBuilder<NowPlayingScrollCubit, NowPlayingScrollState>(
+                        builder: (context, state) {
+                          return Container(
+                            color: Colors.white,
+                            child: Text(
+                              "${state.songPositionString} | ${state.songLengthString}",
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
+                          );
+                        },
+                      ),
                     );
                   },
                 ),
-              );
-            },
-          ),
-          Column(
-            children: [
-              SizedBox(
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(10, 20, 0, 0),
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      BlocBuilder<AudioControllerBloc, AudioControllerState>(
-                        builder: (context, state) {
-                          return Container(
-                            color: Colors.white,
-                            child: MarqueeWidget(
-                              direction: Axis.horizontal,
-                              text: Text(
-                                state.mix!.producer,
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      BlocBuilder<AudioControllerBloc, AudioControllerState>(
-                        // todo: buildWhen
-                        builder: (context, state) {
-                          return Container(
-                            color: Colors.white,
-                            child: MarqueeWidget(
-                              text: Text(
-                                state.mix!.event,
-                                style: Theme.of(context).textTheme.headline6,
-                                textAlign: TextAlign.left,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      BlocBuilder<AudioControllerBloc, AudioControllerState>(
-                        // todo: buildWhen
-                        builder: (context, state) {
-                          return Container(
-                            color: Colors.white,
-                            child: MarqueeWidget(
-                              text: Text(
-                                DateFormat('yyyy-MM-dd').format(state.mix!.dateUploaded),
-                                style: Theme.of(context).textTheme.headline5,
-                                textAlign: TextAlign.left,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                SizedBox(
+                  height: 50,
                 ),
-                height: 300,
-              ),
-              SizedBox(
-                height: 200,
-              ),
-              BlocBuilder<AudioControllerBloc, AudioControllerState>(
-                buildWhen: (prev, current) {
-                  return current.secondsIn == 0;
-                },
-                builder: (context, state) {
-                  return BlocProvider(
-                    create: (context) => NowPlayingScrollCubit(
-                      _scrollController,
-                      state.mix!,
-                    ),
-                    child: BlocBuilder<NowPlayingScrollCubit, NowPlayingScrollState>(
-                      builder: (context, state) {
-                        return Container(
-                          color: Colors.white,
-                          child: Text(
-                            "${state.songPositionString} | ${state.songLengthString}",
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              BlocBuilder<AudioControllerBloc, AudioControllerState>(
-                // todo: this won't work
-                buildWhen: (prev, current) {
-                  return current.secondsIn == 0;
-                },
-                builder: (context, state) {
-                  return BlocProvider(
-                    create: (context) => NowPlayingScrollCubit(
-                      _scrollController,
-                      state.mix!,
-                    ),
-                    child: Container(
-                      height: 100,
-                      child: NowPlayingScrollable(
-                        scrollController: _scrollController,
-                        songLength: state.mix!.length,
+                BlocBuilder<AudioControllerBloc, AudioControllerState>(
+                  // todo: this won't work
+                  buildWhen: (prev, current) {
+                    return current.secondsIn == 0;
+                  },
+                  builder: (context, state) {
+                    return BlocProvider(
+                      create: (context) => NowPlayingScrollCubit(
+                        _scrollController,
+                        state.mix!,
                       ),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ]),
+                      child: Container(
+                        height: 100,
+                        child: NowPlayingScrollable(
+                          scrollController: _scrollController,
+                          songLength: state.mix!.length,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
