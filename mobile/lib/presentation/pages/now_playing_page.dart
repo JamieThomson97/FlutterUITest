6c50b86fe3d1af040f1a8f3999c9ba9a535e4127
Jamie Thomson <jaytadmn@gmail.com>
2021-10-27 22:21:07 +0100

    can't make it work

--- mobile/lib/presentation/pages/now_playing_page.dart
+++ mobile/lib/presentation/pages/now_playing_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:vibration/bloc/audio_controller/audio_controller_bloc.dart';
 import 'package:vibration/cubit/now_playing_scroll/now_playing_scroll_cubit.dart';
 import 'package:vibration/presentation/widgets/marquee.dart';
@@ -244,7 +245,8 @@ class NowPlayingPage extends StatelessWidget {
                           ),
                         ],
                       ),
-                    )
+                    ),
+                    SlideUpWidget(),
                   ],
                 ),
               ],
@@ -253,6 +255,71 @@ class NowPlayingPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class SlideUpWidget extends StatefulWidget {
+  final SlideUpController controller;
+
+  const SlideUpWidget() : super();
+
+  @override
+  _SlideUpWidgetState createState() => _SlideUpWidgetState();
+}
+
+class _SlideUpWidgetState extends State<SlideUpWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => SlideUpProvider(),
+      child: Consumer<SlideUpProvider>(
+        builder: (context, provider, child) {
+          widget.controller?.providerContext = context;
+          return provider.isShow
+              ? Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+                )
+              : Container();
+        },
+      ),
+    );
+  }
+}
+
+class SlideUpProvider with ChangeNotifier {
+  bool isShow = false;
+
+  void updateState(bool newState) {
+    isShow = newState;
+    notifyListeners();
+  }
+}
+
+class SlideUpController {
+  SlideUpController._private();
+
+  static final SlideUpController instance = SlideUpController._private();
+
+  factory SlideUpController() => instance;
+
+  BuildContext _providerContext;
+
+  set providerContext(BuildContext context) {
+    if (_providerContext != context) {
+      _providerContext = context;
+    }
+  }
+
+  void toggle() {
+    if (_providerContext != null) {
+      final provider = _providerContext.read<SlideUpProvider>();
+      provider.updateState(!provider.isShow);
+    } else {
+      print('Need init provider context');
+    }
   }
 }
 
