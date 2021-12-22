3bd5ea4c5997c6bd081adf4cdf8d80a34eadbfc6
Jamie Thomson <jaytadmn@gmail.com>
2021-12-22 15:22:43 +0000

    Add cubit stuff

--- mobile/lib/presentation/pages/search_page.dart
+++ mobile/lib/presentation/pages/search_page.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vibration/cubit/changeable_filter/changeable_filter_cubit.dart';
 import 'package:vibration/presentation/widgets/title_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -19,33 +21,44 @@ class SearchPage extends StatelessWidget {
         children: <Widget>[
           TitleBar(userName: "Jamie"),
           listSpacer,
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey[200],
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                  icon: Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Icon(
-                      Icons.search_sharp,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  border: InputBorder.none,
-                  hintText: 'search',
-                  hintStyle: TextStyle(color: Colors.grey),
-                ),
-              ),
-            ),
-          ),
+          PasswordBox(),
           SizedBox(height: 15),
           ChangeableSearchOptions()
         ],
+      ),
+    );
+  }
+}
+
+class PasswordBox extends StatelessWidget {
+  const PasswordBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.grey[200],
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            icon: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+              child: Icon(
+                Icons.search_sharp,
+                color: Colors.grey,
+              ),
+            ),
+            border: InputBorder.none,
+            hintText: 'search',
+            hintStyle: TextStyle(color: Colors.grey),
+          ),
+        ),
       ),
     );
   }
@@ -61,22 +74,29 @@ class ChangeableSearchOptions extends StatelessWidget {
       child: Container(
         height: 30,
         // color: Colors.black,
-        child: Column(
-          children: [
-            Container(
-              height: 30,
-              child: ListView.separated(
-                separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(width: 10);
-                },
-                itemCount: 3,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  return selectableFilter("Live shows $index");
-                },
-              ),
-            )
-          ],
+        child: BlocProvider(
+          create: (context) => ChangeableFilterCubit(MockFilterGetter()),
+          child: BlocBuilder<ChangeableFilterCubit, ChangeableFilterState>(
+            builder: (context, state) {
+              return Column(
+                children: [
+                  Container(
+                    height: 30,
+                    child: ListView.separated(
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(width: 10);
+                      },
+                      itemCount: 3,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return selectableFilter("Live shows $index");
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
