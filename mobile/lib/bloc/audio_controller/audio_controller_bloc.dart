119f36dd544593509c65c5a9c6af76ea8d4cb185
Jamie Thomson <jaytadmn@gmail.com>
2021-12-22 15:17:15 +0000

    Update AudioControllerBloc

--- mobile/lib/bloc/audio_controller/audio_controller_bloc.dart
+++ mobile/lib/bloc/audio_controller/audio_controller_bloc.dart
@@ -15,64 +15,75 @@ class AudioControllerBloc extends Bloc<AudioControllerEvent, AudioControllerStat
         _durationChanged(event);
       },
     );
+
+    on<MixStartedEvent>((event, emit) => _mixStarted(event, emit));
+    on<MixPlayPausedEvent>((event, emit) => _mixPlayPaused(event, emit));
+    on<MixTimestampChangedEvent>((event, emit) => _mixTimestampChanged(event, emit));
+    on<MixSeekStartedEvent>((event, emit) => _ignoreSeeks = true);
+    on<MixSeekEndedEvent>((event, emit) => _mixSeekEnded(event.songPercentage, emit));
+    on<MixSongJumpedEvent>(
+      (event, emit) {
+        double percentage = event.songStart / state.mix!.length;
+        _mixSeekEnded(percentage, emit);
+      },
+    );
   }
 
   late MusicPlayer _musicPlayer = MusicPlayer(new JustAudioWrapper());
 
-  @override
-  Stream<AudioControllerState> mapEventToState(
-    AudioControllerEvent event,
-  ) async* {
-    if (event is MixStartedEvent) yield* _mixStarted(event);
-    if (event is MixPlayPausedEvent) yield* _mixPlayPaused(event);
-    if (event is MixTimestampChangedEvent) yield* _mixTimestampChanged(event);
-    if (event is MixSeekStartedEvent) yield* _mixSeekStarted(event);
-    if (event is MixSeekEndedEvent) yield* _mixSeekEnded(event.songPercentage);
-    if (event is MixSongJumpedEvent) {
-      double percentage = event.songStart / state.mix!.length;
-      yield* _mixSeekEnded(percentage);
-    }
-    ;
-  }
-
-  Stream<AudioControllerState> _mixStarted(MixStartedEvent event) async* {
-    yield AudioControllerState(
-      mix: event.mix,
-      secondsIn: 0,
-      isPlaying: true,
-      status: AudioControllerStatus.HasSong,
+  Future<void> _mixStarted(
+    MixStartedEvent event,
+    Emitter<AudioControllerState> emit,
+  ) async {
+    emit(
+      AudioControllerState(
+        mix: event.mix,
+        secondsIn: 0,
+        isPlaying: true,
+        status: AudioControllerStatus.HasSong,
+      ),
     );
     await _musicPlayer.initialiseMix(event.mix.path);
     _musicPlayer.playPauseMix();
   }
 
-  Stream<AudioControllerState> _mixPlayPaused(MixPlayPausedEvent event) async* {
-    yield state.copyWith(
-      AudioControllerStatus.HasSong,
-      isPlaying: !_musicPlayer.isPlaying,
+  Future<void> _mixPlayPaused(
+    MixPlayPausedEvent event,
+    Emitter<AudioControllerState> emit,
+  ) async {
+    emit(
+      state.copyWith(
+        AudioControllerStatus.HasSong,
+        isPlaying: !_musicPlayer.isPlaying,
+      ),
     );
     _musicPlayer.playPauseMix();
   }
 
-  Stream<AudioControllerState> _mixTimestampChanged(MixTimestampChangedEvent event) async* {
-    yield state.copyWith(
-      AudioControllerStatus.HasSong,
-      secondsIn: event.secondsIn,
+  Future<void> _mixTimestampChanged(
+    MixTimestampChangedEvent event,
+    Emitter<AudioControllerState> emit,
+  ) async {
+    emit(
+      state.copyWith(
+        AudioControllerStatus.HasSong,
+        secondsIn: event.secondsIn,
+      ),
     );
   }
 
   bool _ignoreSeeks = false;
-
-  Stream<AudioControllerState> _mixSeekStarted(MixSeekStartedEvent event) async* {
-    _ignoreSeeks = true;
-  }
-
-  Stream<AudioControllerState> _mixSeekEnded(double songPercentage) async* {
+  Future<void> _mixSeekEnded(
+    double songPercentage,
+    Emitter<AudioControllerState> emit,
+  ) async {
     var time = Duration(seconds: (state.mix!.length * songPercentage).round());
     _musicPlayer.seekMix(time);
-    yield state.copyWith(
-      AudioControllerStatus.HasSong,
-      secondsIn: (state.mix!.length * songPercentage).round(),
+    emit(
+      state.copyWith(
+        AudioControllerStatus.HasSong,
+        secondsIn: (state.mix!.length * songPercentage).round(),
+      ),
     );
     _ignoreSeeks = false;
   }
